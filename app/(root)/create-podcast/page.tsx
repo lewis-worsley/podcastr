0ac104b0c8872fb8379a25c33d23fd40ca171266
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-05 16:02:34 +0100

    added respective heading for create-podcast & discover

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const CreatePodcast = () => {
     return (
-        <div>CreatePodcast</div>
+        <div className='mt-9 flex flex-col gap-9'>
+            <section className='flex flex-col gap-5'>
+                <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
+            </section>
+        </div>
     )
 }
 
--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Discover = () => {
     return (
-        <div>Discover</div>
+        <div className='mt-9 flex flex-col gap-9'>
+            <section className='flex flex-col gap-5'>
+                <h1 className='text-20 font-bold text-white-1'>Discover</h1>
+            </section>
+        </div>
     )
 }
 

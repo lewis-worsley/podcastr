25c4115b3daea6a2ee56d42d69cfd8585fc433e5
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-06 09:41:50 +0100

    adjusted provider to use Convex with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./providers/ConvexClientProvider";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Podcastr",
-  description: "Generate your podcasts using AI",
-  icons: {
-    icon: '/icons/logo.svg'
-  }
+	title: "Podcastr",
+	description: "Generate your podcasts using AI",
+	icons: {
+		icon: '/icons/logo.svg'
+	}
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
-        </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ConvexClerkProvider>
+					{children}
+				</ConvexClerkProvider>
+			</body>
+		</html>
+	);
 }

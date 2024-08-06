4854e732f988f2429a1e79d9002d3cd17f3e6701
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-06 11:22:18 +0100

    adjusted appearance to match brand and connected clerk with nextjs

--- app/providers/ConvexClerkProvider.tsx
+++ app/providers/ConvexClerkProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
@@ -8,7 +8,22 @@ import { ReactNode } from "react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const ConvexClerkProvider = ({children}: {children: ReactNode}) => (
-		<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+		<ClerkProvider 
+			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+			appearance={{
+				layout: {
+					socialButtonsVariant: 'iconButton',
+					logoImageUrl: '/icons/auth-logo.svg',
+				},
+				variables: {
+					colorBackground: '#15171C',
+					colorPrimary: '',
+					colorText: '#FFF',
+					colorInputBackground: '#1B1F29',
+					colorInputText: '#FFF',
+				}
+			}}
+		>
 			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
 				{children}
 			</ConvexProviderWithClerk>

075252ff9f5dc5f5176817063146e3718cc5b35b
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-06 09:41:11 +0100

    set up auth config with Convex & Clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+        {
+            domain: "https://eager-moccasin-35.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+export default authConfig

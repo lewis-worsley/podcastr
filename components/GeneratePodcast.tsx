9f7c8aa3d2df82a4e05a0ea268dd48ccefb99f19
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-16 17:08:16 +0100

    wip: adds AI Prompt textarea

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,5 +1,7 @@
 import { GeneratePodcastProps } from '@/types'
 import React from 'react'
+import { Label } from './ui/label'
+import { Textarea } from './ui/textarea'
 
 const GeneratePodcast = ({
     setAudioStorageId,
@@ -11,7 +13,20 @@ const GeneratePodcast = ({
     setAudioDuration,
 }: GeneratePodcastProps) => {
     return (
-        <div>GeneratePodcast</div>
+        <div>
+            <div className='flex flex-col gap-2.5'>
+                <Label className='text-16 font-bold text-white-1'>
+                    AI Prompt to generate Podcast
+                </Label>
+                <Textarea
+                    className='input-class font-light focus-visible:ring-offset-orange-1'
+                    placeholder='Provide text to generate audio'
+                    rows={5}
+                    value={voicePrompt}
+                    onChange={(e) => setVoicePrompt(e.target.value)}
+                />
+            </div>
+        </div>
     )
 }
 

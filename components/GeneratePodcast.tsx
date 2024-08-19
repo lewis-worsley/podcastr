e422b2eea5a3bf84164cd49abc265ce45b99e704
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-08-19 15:58:23 +0100

    wip: set up convex functions for file upload for GeneratePodcast

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,21 +1,55 @@
-import { GeneratePodcastProps } from '@/types'
-import React, { useState } from 'react'
-import { Label } from './ui/label'
-import { Textarea } from './ui/textarea'
-import { Button } from './ui/button'
-import { Loader } from 'lucide-react'
+import { GeneratePodcastProps } from '@/types';
+import React, { useState } from 'react';
+import { Label } from './ui/label';
+import { Textarea } from './ui/textarea';
+import { Button } from './ui/button';
+import { Loader } from 'lucide-react';
+import { useAction, useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { v4 as uuidv4 } from 'uuid';
+import { generateUploadUrl } from '@/convex/files';
+import { useUploadFiles } from "@xixixao/uploadstuff/react";
 
-const GeneratePodcast = ({
-    setAudioStorageId,
-    setAudio,
-    voiceType,
-    audio,
-    voicePrompt,
-    setVoicePrompt,
-    setAudioDuration,
-}: GeneratePodcastProps) => {
-
+const useGeneratePodcast = ({ setAudio, voiceType, voicePrompt, setAudioStorageId }: GeneratePodcastProps) => {
     const [isGenerating, setIsGenerating] = useState(false);
+
+    const generateUploadUrl = useMutation(api.files.generateUploadUrl)
+    const { startUpload } = useUploadFiles(generateUploadUrl);
+
+    const getPodcastAudio = useAction(api.openai.generateAudioAction)
+
+    const generatePodcast = async () => {
+        setIsGenerating(true);
+        setAudio('');
+
+        if (!voicePrompt) {
+            // todo: show error message
+            return setIsGenerating(false);
+        }
+
+        try {
+            const response = await getPodcastAudio({
+                voice: voiceType,
+                input: voicePrompt,
+            });
+
+            const blob = new Blob([response], { type: 'audio/mpeg' });
+            const fileName = `podcast-${uuidv4()}.mp3`;
+            const file = new File([blob], fileName, { type: 'audio/mpeg' });
+
+
+        } catch (error) {
+            console.error('Error generating podcast', error)
+            setIsGenerating(false);
+        }
+    }
+
+
+    return { isGenerating, generatePodcast };
+};
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+    const { isGenerating, generatePodcast } = useGeneratePodcast(props);
 
     return (
         <div>
@@ -27,12 +61,12 @@ const GeneratePodcast = ({
                     className='input-class font-light focus-visible:ring-offset-orange-1'
                     placeholder='Provide text to generate audio'
                     rows={5}
-                    value={voicePrompt}
-                    onChange={(e) => setVoicePrompt(e.target.value)}
+                    value={props.voicePrompt}
+                    onChange={(e) => props.setVoicePrompt(e.target.value)}
                 />
             </div>
             <div className='mt-5 w-full max-w-[200px]'>
-                <Button 
+                <Button
                     type='submit'
                     className='text-16 bg-orange-1 py-4 font-bold text-white-1'
                 >
@@ -46,8 +80,17 @@ const GeneratePodcast = ({
                     )}
                 </Button>
             </div>
+            {props.audio && (
+                <audio
+                    controls
+                    src={props.audio}
+                    autoPlay
+                    className='mt-5'
+                    onLoadedMetadata={(e) => props.setAudioDuration(e.currentTarget.duration)}
+                />
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default GeneratePodcast
+export default GeneratePodcast;

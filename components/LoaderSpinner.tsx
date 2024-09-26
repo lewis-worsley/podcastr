9351a2ab34def0da1f20e888f0ca0b2761617600
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-09-26 16:12:09 +0100

    added size to LoaderSpinner component

--- components/LoaderSpinner.tsx
+++ components/LoaderSpinner.tsx
@@ -3,7 +3,7 @@ import { Loader } from 'lucide-react'
 const LoaderSpinner = () => {
     return (
         <div className='flex-center h-screen w-full'>
-            <Loader className='animate-spin text-orange-1' />
+            <Loader className='animate-spin text-orange-1' size={30} />
         </div>
     )
 }

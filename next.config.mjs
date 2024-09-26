b90f9bb420add5d1d1e28e57adfc42671393100c
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-09-26 17:01:14 +0100

    added new hostname so avatars can display

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'descriptive-hippopotamus-64.convex.cloud'
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com'
             }
         ]
     }

c06ba4e676ceaf0aed3cb698cb9b01e633ba1743
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-09-26 16:13:12 +0100

    created basic layout for podcast details and empty state

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import EmptyState from '@/components/EmptyState';
+import LoaderSpinner from '@/components/LoaderSpinner';
+import PodcastCard from '@/components/PodcastCard';
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { useQuery } from 'convex/react'
@@ -8,6 +11,9 @@ import React from 'react'
 
 const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'podcasts'> } }) => {
     const podcast = useQuery(api.podcasts.getPodcastById, { podcastId })
+    const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, { podcastId })
+
+    if (!similarPodcasts || !podcast) return <LoaderSpinner />
 
     return (
         <section className='flex w-full flex-col'>
@@ -22,11 +28,49 @@ const PodcastDetails = ({ params: { podcastId } }: { params: { podcastId: Id<'po
                         height={24}
                         alt='headphone'
                     />
-                    <h2 className='text-16 font-bold text-white-1'>
+                    <div className='text-16 font-bold text-white-1'>
                         {podcast?.views}
-                    </h2>
+                    </div>
                 </figure>
             </header>
+            <p className='text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center'>
+                {podcast?.podcastDescription}
+            </p>
+            <div className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-4'>
+                    <h2 className='text-18 font-bold text-white-1'>Transcription</h2>
+                    <p className='text-16 font-medium text-white-2'>{podcast?.voicePrompt}</p>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <h2 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h2>
+                    <p className='text-16 font-medium text-white-2'>{podcast?.imagePrompt}</p>
+                </div>
+            </div>
+            <section className='mt-8 flex flex-col gap-5'>
+                <h2 className='text-18 font-bold text-white-1'>Similar Podcasts</h2>
+                {similarPodcasts && similarPodcasts.length > 
+                0 ? (
+                    <div className='podcast_grid'>
+                        {similarPodcasts.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                            <PodcastCard 
+                                key={_id}
+                                imgUrl={imageUrl!}
+                                title={podcastTitle}
+                                description={podcastDescription}
+                                podcastId={_id}
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <>
+                        <EmptyState 
+                            title="No similiar podcasts found"
+                            buttonLink="/discover"
+                            buttonText="Discover more podcasts"
+                        />
+                    </>
+                )}
+            </section>
         </section>
     )
 }

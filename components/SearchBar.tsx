b33c79e424daad68576f9f9e32fea3a66e0a157b
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-10-16 23:26:35 +0100

    wip: initial SearchBar component created

---
+++ components/SearchBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import { Input } from './ui/input';
+import Image from 'next/image';
+import { usePathname, useRouter } from 'next/navigation';
+
+const SearchBar = () => {
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+    const pathname = usePathname();
+
+    useEffect(() => {
+        if (search) {
+            router.push(`/discover?search=${search}`)
+        } else if (!search && pathname === "/discover") {
+            router.push("/discover");
+        }
+    }, [router, pathname, search])
+
+    return (
+        <div className='relative mt-8 block'>
+            <Input
+                className='input-class py-6 pl-12 focus-visible:ring-offset-orange-1'
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder='Search for podcasts'
+                onLoad={() => setSearch("")}
+            />
+            <Image
+                src="/icons/search.svg"
+                alt='search'
+                height={20}
+                width={20}
+                className='absolute left-4 top-3.5'
+            />
+        </div>
+    )
+}
+
+export default SearchBar

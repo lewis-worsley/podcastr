3e698ce4f07449d775c68133d81e73d1c8e3f770
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-10-18 00:33:30 +0100

    chore: tidied files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import LoaderSpinner from '@/components/LoaderSpinner';
 import PodcastCard from '@/components/PodcastCard';
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
@@ -6,17 +8,19 @@ import { useQuery } from "convex/react";
 const Home = () => {
 	const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
 
+	if (!trendingPodcasts) return <LoaderSpinner />
+
 	return (
-		<div className='mt-9 flex flex-col gap-9'>
+		<div className="mt-9 flex flex-col gap-9 md:overflow-hidden">
 			<section className='flex flex-col gap-5'>
-				<h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
-				<div className='podcast_grid'>
+				<h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+				<div className="podcast_grid">
 					{trendingPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
 						<PodcastCard
 							key={_id}
+							imgUrl={imageUrl as string}
 							title={podcastTitle}
 							description={podcastDescription}
-							imgUrl={imageUrl!}
 							podcastId={_id}
 						/>
 					))}

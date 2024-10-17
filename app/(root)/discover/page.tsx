a2c7fb5957a53769890286bce211054e2e4f1689
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-10-17 11:23:30 +0100

    filters search results for podcasts

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -6,10 +6,9 @@ import PodcastCard from '@/components/PodcastCard';
 import SearchBar from '@/components/SearchBar';
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
-import React from 'react';
 
-const Discover = () => {
-    const podcastsData = useQuery(api.podcasts.getPodcastBySearch, { search: "" })
+const Discover = ({ searchParams: { search } }: { searchParams: { search: string } }) => {
+    const podcastsData = useQuery(api.podcasts.getPodcastBySearch, { search: search || "" })
 
     return (
         <div className='flex flex-col gap-9'>

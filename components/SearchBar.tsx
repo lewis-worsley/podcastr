6709d93657e163261092595edfb85828bdc85d50
Lewis-CaS <lewis.worsley@clickasnap.com>
2024-10-17 11:30:02 +0100

    ensures debounce is active after a half second delay in search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,19 +4,21 @@ import React, { useEffect, useState } from 'react';
 import { Input } from './ui/input';
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
+import { useDebounce } from '@/lib/useDebounce';
 
 const SearchBar = () => {
     const [search, setSearch] = useState("");
     const router = useRouter();
     const pathname = usePathname();
+    const debounceValue = useDebounce(search, 500);
 
     useEffect(() => {
-        if (search) {
-            router.push(`/discover?search=${search}`)
-        } else if (!search && pathname === "/discover") {
+        if (debounceValue) {
+            router.push(`/discover?search=${debounceValue}`)
+        } else if (!debounceValue && pathname === "/discover") {
             router.push("/discover");
-        }
-    }, [router, pathname, search])
+        }debounceValue
+    }, [router, pathname, debounceValue])
 
     return (
         <div className='relative mt-8 block'>
